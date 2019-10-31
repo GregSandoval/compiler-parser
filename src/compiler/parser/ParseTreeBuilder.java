d90528c9ff3629824d44e35608296e6e6a617673
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2019-10-30 20:07:08 -0700

    Refactor builder to use lambdas.

--- src/compiler/parser/ParseTreeBuilder.java
+++ src/compiler/parser/ParseTreeBuilder.java
@@ -5,25 +5,19 @@ import compiler.lexer.token.Token;
 import java.util.List;
 
 public class ParseTreeBuilder {
-  public ParseTreeBuilderLastStep setStartSymbol(GrammarNode startSymbol) {
-    return new ParseTreeBuilderLastStep(startSymbol);
-  }
-
-  public static class ParseTreeBuilderLastStep {
-    private GrammarNode startSymbol;
-
-    private ParseTreeBuilderLastStep(GrammarNode startSymbol) {
-      this.startSymbol = startSymbol;
-    }
-
-    public AbstractGrammarNode build(List<Token> tokens) throws Exception {
+  public ParseTreeBuilderBuildStep setStartSymbol(GrammarNode startSymbol) {
+    return tokens -> {
       final var builderStrategy = new ParseTreeStrategy();
       new ParserBuilder()
-        .setStartSymbol(this.startSymbol)
+        .setStartSymbol(startSymbol)
         .onGrammarRuleApplication(builderStrategy)
         .createParser()
         .parse(tokens);
       return builderStrategy.getRoot();
-    }
+    };
+  }
+
+  public interface ParseTreeBuilderBuildStep {
+    AbstractGrammarNode build(List<Token> tokens) throws Exception;
   }
 }
