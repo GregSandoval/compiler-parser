ee6c4774946d3cf62cec11459ce8a5f1241a4678
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2019-11-07 18:01:24 -0800

    Resolve error with hoisting.

--- src/compiler/a5/grammar/PstToAstGrammarVisitor.java
+++ src/compiler/a5/grammar/PstToAstGrammarVisitor.java
@@ -1,10 +1,11 @@
 package compiler.a5.grammar;
 
+import compiler.parser.GrammarNode;
+
 import static compiler.a5.grammar.A5GrammarNonTerminals.*;
 import static compiler.parser.PstToAstHelpers.hoist;
 
 public class PstToAstGrammarVisitor implements GrammarNodeVisitor {
-
   @Override
   public void visit(Expr tree) {
     if (tree.children.size() == 2) {
@@ -72,6 +73,371 @@ public class PstToAstGrammarVisitor implements GrammarNodeVisitor {
   @Override
   public void visit(PPexpr ppexpr) {
     hoist(ppexpr);
+  }
+
+  @Override
+  public void visit(Classmom node) {
+
+  }
+
+  @Override
+  public void visit(GrammarNode node) {
+
+  }
+
+  @Override
+  public void visit(Pgm node) {
+
+  }
+
+  @Override
+  public void visit(Main node) {
+
+  }
+
+  @Override
+  public void visit(BBlock node) {
+
+  }
+
+  @Override
+  public void visit(Vargroup node) {
+
+  }
+
+  @Override
+  public void visit(PPvarlist node) {
+
+  }
+
+  @Override
+  public void visit(Varlist node) {
+
+  }
+
+  @Override
+  public void visit(Varitem node) {
+
+  }
+
+  @Override
+  public void visit(Varitem_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Vardecl node) {
+
+  }
+
+  @Override
+  public void visit(Simplekind node) {
+
+  }
+
+  @Override
+  public void visit(BaseKind node) {
+
+  }
+
+  @Override
+  public void visit(Varspec node) {
+
+  }
+
+  @Override
+  public void visit(Varid node) {
+
+  }
+
+  @Override
+  public void visit(Arrspec node) {
+
+  }
+
+  @Override
+  public void visit(KKint node) {
+
+  }
+
+  @Override
+  public void visit(Deref_id node) {
+
+  }
+
+  @Override
+  public void visit(Deref node) {
+
+  }
+
+  @Override
+  public void visit(Varinit node) {
+
+  }
+
+  @Override
+  public void visit(BBexprs node) {
+
+  }
+
+  @Override
+  public void visit(Exprlist node) {
+
+  }
+
+  @Override
+  public void visit(Moreexprs node) {
+
+  }
+
+  @Override
+  public void visit(Classdecl node) {
+
+  }
+
+  @Override
+  public void visit(Classdef node) {
+
+  }
+
+  @Override
+  public void visit(Classdef_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(BBClassitems node) {
+
+  }
+
+  @Override
+  public void visit(Classitems node) {
+
+  }
+
+  @Override
+  public void visit(Classgroup node) {
+
+  }
+
+  @Override
+  public void visit(Class_ctrl node) {
+
+  }
+
+  @Override
+  public void visit(Interfaces node) {
+
+  }
+
+  @Override
+  public void visit(Mddecls node) {
+
+  }
+
+  @Override
+  public void visit(Mdheader node) {
+
+  }
+
+  @Override
+  public void visit(Md_id node) {
+
+  }
+
+  @Override
+  public void visit(Fcndefs node) {
+
+  }
+
+  @Override
+  public void visit(Fcndef node) {
+
+  }
+
+  @Override
+  public void visit(Fcnheader node) {
+
+  }
+
+  @Override
+  public void visit(Fcnid node) {
+
+  }
+
+  @Override
+  public void visit(Retkind node) {
+
+  }
+
+  @Override
+  public void visit(PParmlist node) {
+
+  }
+
+  @Override
+  public void visit(Varspecs node) {
+
+  }
+
+  @Override
+  public void visit(More_varspecs node) {
+
+  }
+
+  @Override
+  public void visit(PPonly node) {
+
+  }
+
+  @Override
+  public void visit(Stmts node) {
+
+  }
+
+  @Override
+  public void visit(Stmt node) {
+
+  }
+
+  @Override
+  public void visit(StasgnOrFcall node) {
+
+  }
+
+  @Override
+  public void visit(StasgnOrFcall_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Stasgn_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Lval_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Lval node) {
+
+  }
+
+  @Override
+  public void visit(LvalOrFcall node) {
+
+  }
+
+  @Override
+  public void visit(LvalOrFcall_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Lval_Tail node) {
+
+  }
+
+  @Override
+  public void visit(KKexpr node) {
+
+  }
+
+  @Override
+  public void visit(Fcall node) {
+
+  }
+
+  @Override
+  public void visit(PPexprs node) {
+
+  }
+
+  @Override
+  public void visit(Stif node) {
+
+  }
+
+  @Override
+  public void visit(Elsepart node) {
+
+  }
+
+  @Override
+  public void visit(Stwhile node) {
+
+  }
+
+  @Override
+  public void visit(Stprint node) {
+
+  }
+
+  @Override
+  public void visit(Strtn node) {
+
+  }
+
+  @Override
+  public void visit(Strtn_Suffix node) {
+
+  }
+
+  @Override
+  public void visit(Fact node) {
+
+  }
+
+  @Override
+  public void visit(BaseLiteral node) {
+
+  }
+
+  @Override
+  public void visit(Addrof_id node) {
+
+  }
+
+  @Override
+  public void visit(Oprel node) {
+
+  }
+
+  @Override
+  public void visit(Lthan node) {
+
+  }
+
+  @Override
+  public void visit(Gthan node) {
+
+  }
+
+  @Override
+  public void visit(Opadd node) {
+
+  }
+
+  @Override
+  public void visit(Opmul node) {
+
+  }
+
+  @Override
+  public void visit(Epsilon node) {
+
+  }
+
+  @Override
+  public void visit(Classheader node) {
+
+  }
+
+  @Override
+  public void visit(Classid node) {
+
   }
 
 }
--- src/compiler/parser/PstToAstHelpers.java
+++ src/compiler/parser/PstToAstHelpers.java
@@ -6,31 +6,33 @@ public class PstToAstHelpers {
 
   public static void hoist(AbstractGrammarNode tree) {
     for (var token : tree.children) {
-      if (token instanceof Token) {
-        final var parent = tree.parent;
-        var tokenIndex = parent.children.indexOf(tree);
-
-        // Replace token's parent
-        token.parent = parent;
-
-        // Replace rule with token
-        parent.children.set(tokenIndex, token);
-
-        // Remove token from rule's children
-        tree.children.remove(token);
-
-        // Set tree's children to point to token as new parent
-        tree.children.forEach(child -> child.parent = token);
-
-        // Add rule's children to the token;
-        while (--tokenIndex != 0 && !tree.children.isEmpty())
-          token.children.addFirst(tree.children.pop());
-
-        while (!tree.children.isEmpty())
-          token.children.addLast(tree.children.pop());
-
-        return;
+      if (!(token instanceof Token)) {
+        continue;
       }
+
+      final var parent = tree.parent;
+      var tokenIndex = parent.children.indexOf(tree);
+
+      // Replace token's parent
+      token.parent = parent;
+
+      // Replace rule with token
+      parent.children.set(tokenIndex, token);
+
+      // Remove token from rule's children
+      tree.children.remove(token);
+
+      // Set tree's children to point to token as new parent
+      tree.children.forEach(child -> child.parent = token);
+
+      // Add rule's children to the token;
+      while (--tokenIndex != 0 && !tree.children.isEmpty())
+        token.children.addLast(tree.children.pop());
+
+      while (!tree.children.isEmpty())
+        token.children.addLast(tree.children.pop());
+
+      return;
     }
   }
 }
