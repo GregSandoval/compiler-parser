cedb0f004d955bb31a10e78018d5c38838981f98
Gregory A. Sandoval <gregorysandoval.a@gmail.com>
2019-11-17 19:55:57 -0800

    Use custom dot file generator;

--- src/visualization/TreeVisualizer.java
+++ src/visualization/TreeVisualizer.java
@@ -1,26 +1,13 @@
 package visualization;
 
-import compiler.lexer.token.EOFToken;
-import compiler.lexer.token.StringToken;
-import compiler.lexer.token.Token;
 import compiler.parser.AbstractGrammarNode;
-import visualization.dotfile.Digraph;
+import visualization.dotfile.TreeDotFileSerializer;
 
 import java.io.IOException;
 
 public class TreeVisualizer {
-  private static String NULL_NODE = "NULL_NODE";
-
   public static void toImage(AbstractGrammarNode tree, String name) throws IOException, InterruptedException {
-    final var graph = new Digraph("testing");
-
-    graph.addNode(NULL_NODE, "NULL_NODE");
-
-    buildGraph(graph, tree);
-
-    graph.link(tree.UUID, NULL_NODE);
-    graph.link(NULL_NODE, tree.UUID);
-    graph.generate(name + ".dot");
+    TreeDotFileSerializer.serialize(tree, name + ".dot");
 
     var p = new ProcessBuilder("dot", "-Tpng", name + ".dot", "-o", name + ".png")
       .start();
@@ -34,45 +21,4 @@ public class TreeVisualizer {
     System.out.println("Generated parse tree image, file name: " + name + ".png");
   }
 
-  private static void buildGraph(Digraph graph, AbstractGrammarNode current) {
-    if (current == null) {
-      return;
-    }
-
-    if (graph.exists(current.UUID)) {
-      throw new RuntimeException("Cycle exists, Node: " + current);
-    }
-
-    graph.addNode(current.UUID, formatWithValue(current));
-
-    for (final var child : current.children) {
-      buildGraph(graph, child);
-      graph.link(current.UUID, child.UUID);
-
-      if (child.parent == null) {
-        graph.link(child.UUID, NULL_NODE);
-        continue;
-      }
-
-      if (!graph.exists(child.parent.UUID)) {
-        graph.addNode(child.parent.UUID, formatWithValue(child.parent) + "[Parent] (Error: Missing)");
-        graph.link(child.UUID, child.parent.UUID);
-      } else {
-        graph.link(child.UUID, child.parent.UUID);
-      }
-    }
-  }
-
-  private static String formatWithValue(AbstractGrammarNode rule) {
-    if (rule instanceof EOFToken)
-      return "EOF";
-
-    if(rule instanceof StringToken)
-      return "\'\'" + ((StringToken) rule).getValue() + "\'\'";
-
-    if (rule instanceof Token)
-      return ((Token) rule).getValue();
-
-    return rule.toString();
-  }
 }
